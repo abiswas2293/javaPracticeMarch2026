ea99a46c29712ea71d5ae1df69a4167450d11c23
ann <abiswas2293@yahoo.com>
2026-03-12 10:31:03 +0000

    add code for interface

---
+++ src/main/java/Interface/B.java
@@ -0,0 +1,21 @@
+package Interface;
+
+public class B implements A{
+    public void age(){
+        System.out.println("Age is 20");
+    }
+
+    public void name(){
+        System.out.println("Name is Annie");
+    }
+
+    public void school(){
+        System.out.println("School name is Brojobala");
+    }
+
+    public  void method(){
+        System.out.println("XYZ");
+    }
+
+
+}
